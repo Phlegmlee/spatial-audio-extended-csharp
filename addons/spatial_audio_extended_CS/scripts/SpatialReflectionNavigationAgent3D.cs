6f48b1416c63556beadac13fabf2942d26251177
Tanner <152333980+Phlegmlee@users.noreply.github.com>
2026-05-28 16:08:08 -0600

    RENAME: Spacial -> Spatial

---
+++ addons/spatial_audio_extended_CS/scripts/SpatialReflectionNavigationAgent3D.cs
@@ -0,0 +1,7 @@
+using Godot;
+namespace SpatialAudioCS;
+
+[Tool, Icon("addons/spatial_audio_extended_CS/assets/spatial_reflection_navigation_agent_3d.svg")]
+public partial class SpatialReflectionNavigationAgent3D : Node3D
+{
+}
