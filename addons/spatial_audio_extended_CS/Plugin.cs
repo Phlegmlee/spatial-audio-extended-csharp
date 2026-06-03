ad54f17e38ae0adbcb1224415308977cf8665c5c
Tanner <152333980+Phlegmlee@users.noreply.github.com>
2026-06-03 13:23:36 -0600

    REWRITE: Plugin.cs - Finished

--- addons/spatial_audio_extended_CS/Plugin.cs
+++ addons/spatial_audio_extended_CS/Plugin.cs
@@ -5,7 +5,7 @@ namespace SpatialAudioCS;
 [Tool]
 public partial class Plugin : EditorPlugin
 {
-	private Button AddAcousticButton = null;
+	private Button AcousticButton = null;
 
 	EditorInterface editorInterface = null;
 
@@ -17,14 +17,14 @@ public partial class Plugin : EditorPlugin
 
 	public override void _ExitTree()
 	{
-		// TODO: RemoveButton()
-		// TODO: Disconnect from selection signal.
+		RemoveButton();
+		editorInterface.GetSelection().SelectionChanged -= OnSelectionChanged;
 	}
 
 	private void OnSelectionChanged()
 	{
 		Godot.Collections.Array<Node> selections = editorInterface.GetSelection().GetSelectedNodes();
-		
+
 		if (selections.Count == 1 && selections[0] is CollisionObject3D || selections[0] is CsgShape3D)
 		{
 			Node3D selected = selections[0] as Node3D;
@@ -34,20 +34,75 @@ public partial class Plugin : EditorPlugin
 				AddButton(selected);
 				return;
 			}
-			RemoveButton();
 		}
+		RemoveButton();
 	}
 
 	private void AddButton(Node3D target)
 	{
-		
+		if (AcousticButton != null) return;
+
+		Texture2D srcT = GD.Load<Texture2D>("uid://ces4kkaimtua8");
+		Image img = srcT.GetImage();
+		img.Resize(16, 16, Image.Interpolation.Lanczos);
+
+		AcousticButton = new()
+		{
+			Text = nameof(AcousticBody),
+			Flat = true,
+			TooltipText = "Add an AcousticBody child to this node.",
+			Icon = ImageTexture.CreateFromImage(img),
+		};
+		AcousticButton.AddThemeFontSizeOverride("font_size", 15);
+
+		StyleBoxEmpty emptyStyle = new();
+		AcousticButton.AddThemeStyleboxOverride("normal", emptyStyle);
+		AcousticButton.AddThemeStyleboxOverride("hover", emptyStyle);
+		AcousticButton.AddThemeStyleboxOverride("pressed", emptyStyle);
+		AcousticButton.AddThemeStyleboxOverride("focus", emptyStyle);
+		AcousticButton.AddThemeConstantOverride("h_separation", 4);
+
+		AcousticButton.Pressed += () => OnAcousticButtonPressed(target);
+
+		AddControlToContainer(CustomControlContainer.SpatialEditorMenu, AcousticButton);
 	}
 
 	private void RemoveButton()
 	{
-		
+		if (AcousticButton == null) return;
+		RemoveControlFromContainer(CustomControlContainer.SpatialEditorMenu, AcousticButton);
+		AcousticButton.QueueFree();
+		AcousticButton = null;
 	}
 
-	// TODO: private void OnAddAcousticButtonPressed(Node3D target)
+	private void OnAcousticButtonPressed(Node3D target)
+	{
+		if (target == null || !IsInstanceValid(target))
+		{
+			RemoveButton();
+			return;
+		}
+
+		if (AcousticBody.IsAcousticBodyOnNode(target))
+		{
+			RemoveButton();
+			return;
+		}
+
+		EditorUndoRedoManager undoRedo = GetUndoRedo();
+		AcousticBody aB = new() { Name = nameof(AcousticBody) };
+
+		undoRedo.CreateAction($"Add {nameof(AcousticBody)}");
+		undoRedo.AddDoMethod(target, Node.MethodName.AddChild, aB, true);
+		undoRedo.AddDoMethod(aB, Node.MethodName.SetOwner, editorInterface.GetEditedSceneRoot());
+		undoRedo.AddDoReference(aB);
+		undoRedo.AddUndoMethod(target, Node.MethodName.RemoveChild, aB);
+		undoRedo.CommitAction();
+
+		editorInterface.GetSelection().Clear();
+		editorInterface.GetSelection().AddNode(aB);
+
+		RemoveButton();
+	}
 }
 #endif
